dc8fc90596427332ae307b43fd636d072ea6d3de
Janis Rullis <janis.rullis.it@gmail.com>
2020-03-29 14:35:22 +0300

    Update Sample-table-with-common-fields.sql

--- mysql/Sample-table-with-common-fields.sql
+++ mysql/Sample-table-with-common-fields.sql
@@ -1,14 +1,12 @@
---  Sample table with common fields.
-
-CREATE TABLE `partner`(
-    `id` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `partner`(
+    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(250) NOT NULL,
     `url` VARCHAR(250) NOT NULL,
     `is_public` ENUM('y', 'n') NOT NULL DEFAULT 'n' COMMENT 'ENUM is more readable, stricter and faster than VARCHAR.',
     `is_visible` ENUM('y', 'n') NOT NULL DEFAULT 'n' COMMENT 'Show in menu?',
     `has_auth` enum('y','n') NOT NULL DEFAULT 'n' COMMENT 'Does the page has authorization?',
     `status` ENUM('waiting', 'in_progress', 'completed') NOT NULL DEFAULT 'waiting',
-    `price` INT(10) UNSIGNED DEFAULT NULL COMMENT "Base price should always be a long INT field, so any time could add a higher precision than 2 digits,  easily make conversions and calc.",
+    `price` SMALLINT(5) UNSIGNED DEFAULT NULL COMMENT "Base price should always be a long INT field, so any time could add a higher precision than 2 digits,  easily make conversions and calc.",
     `mirror_id` INT(10) UNSIGNED NOT NULL COMMENT 'Same item in multiple languages.',
     `country` CHAR(2) NOT NULL,
     `seq` TINYINT(2) UNSIGNED NOT NULL DEFAULT '1',
@@ -24,5 +22,6 @@ CREATE TABLE `partner`(
      `sys_info` varchar(20) DEFAULT NULL COMMENT 'In case if You need to mark it or add some flag. For inner use. For example, edited 3 JSONs by hand and mark \"#1234 upd\", so they could be identified later.',
               
     PRIMARY KEY(`id`),
-    UNIQUE INDEX `title`(`title`)
+    UNIQUE INDEX `title`(`title`),
+    INDEX `owner_order` (`owner_id`, `order_id`)
 ) ENGINE = INNODB DEFAULT CHARACTER SET = UTF8;
